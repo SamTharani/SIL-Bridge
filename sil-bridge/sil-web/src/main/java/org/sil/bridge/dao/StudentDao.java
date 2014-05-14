d82ae0ce43093d6d2a4bb2d979a97c89422fe6ae
Mayooran <smayoorans@gmail.com>
2014-05-14 15:11:25 -0700

    Fixed issue in session  factory beans

--- sil-bridge/sil-web/src/main/java/org/sil/bridge/dao/StudentDao.java
+++ sil-bridge/sil-web/src/main/java/org/sil/bridge/dao/StudentDao.java
@@ -6,7 +6,10 @@ import org.sil.bridge.model.Student;
 import java.util.List;
 
 public interface StudentDao {
+
     public void addStudent(Student student);
+
     public List listStudent();
+
     public void removeStudent(Integer id);
 }
--- sil-bridge/sil-web/src/main/java/org/sil/bridge/service/StudentService.java
+++ sil-bridge/sil-web/src/main/java/org/sil/bridge/service/StudentService.java
@@ -6,7 +6,11 @@ import org.sil.bridge.model.Student;
 import java.util.List;
 
 public interface StudentService {
+
     public void addStudent(Student student);
+
     public List<Student> listStudent();
+
     public void removeStudent(Integer id);
 }
+
